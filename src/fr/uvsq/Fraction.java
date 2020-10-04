e207c895a85543bc1a9a14dde043e11190444fd9
Chloé Helain <chloe.helain@gmail.com>
2020-10-04 14:56:14 +0200

    envoi sans le readme

---
+++ src/fr/uvsq/Fraction.java
@@ -0,0 +1,17 @@
+package fr.uvsq;
+
+public class Fraction {
+    int numerateur;
+    int denominateur;
+
+    Fraction(int newNum, int newDenom){
+        numerateur=newNum;
+        denominateur=newDenom;
+    }
+
+    public String toString(){
+        String resultat = numerateur+"/"+denominateur;
+        return resultat;
+
+    }
+}
---
+++ src/fr/uvsq/Main.java
@@ -0,0 +1,9 @@
+package fr.uvsq;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Fraction formeFractionnaire = new Fraction(5,6);
+        System.out.println(formeFractionnaire.toString());
+    }
+}
